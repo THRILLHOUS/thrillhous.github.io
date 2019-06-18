a760a931c741091f4a553d8f358c3fc7269e7a0d
Justin <justinpatrickmcgee@gmail.com>
2019-06-18 00:15:56 -0700

    Lots of error fixing

--- js/loader.js
+++ js/loader.js
@@ -1,13 +1,14 @@
-const loader - document.querySelector('.loader');
-const main - document.querySelector('.main')
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main')
 
 function init() {
   setTimeout(() => {
-      loader.style.opacity = 0;
-      loader.style.display = 'none';
+    loader.style.opacity = 0;
+    loader.style.display = 'none';
 
-      main.style.display = 'block';
-      setTimeout(() => main.style.opacity = 1), 50);
-  }, 4000) = loader
-const loader:
+    main.style.display = 'block';
+    setTimeout(() => (main.style.opacity = 1), 50);
+  }, 4000);
 }
+
+init();
